a8af4de82004200d0240da3300a27326c6cf9885
Alex Studer <Alex.Studer@ibm.com>
2023-10-16 15:48:16 +0200

    Initial bande_mm with blocking

--- src/main.py
+++ src/main.py
@@ -4,29 +4,106 @@ Experimental file to try out new things
 """
 from dataclasses import dataclass
 
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 
 from matrix_utils import banded_matrix_generator
 from matrix_mm import naive_banded_mm
 
-# na
-A = banded_matrix_generator(8, 3, 3)
-B = banded_matrix_generator(8, 2, 2)
 
-H = naive_banded_mm(A, 3, 3, B, 2, 2)
-T = np.matmul(A,B)
+#A = banded_matrix_generator(8, 3, 3)
+#B = banded_matrix_generator(8, 2, 2)
 
-print("Diff\n", H-T)
+#H = naive_banded_mm(A, 3, 3, B, 2, 2)
+#T = np.matmul(A,B)
+
+#print("Diff\n", H-T)
 #print("H\n", H, "\nT\n", T)
 
 #####################################################
 # Naive block banded matrix multiplication
 # C = alpha*A*B + beta*C
 #####################################################
+def blocking(matrix: np.ndarray, block_size: int):
+    row, col = matrix.shape
+    row_blocks, col_blocks = math.ceil(row/block_size), math.ceil(col/block_size)
+    blocked_matrix = []
+    
+    print(matrix)
+    for i in range(row_blocks):
+        blocked_rows = []
+        for j in range(col_blocks):
+            A = matrix[
+                (i*block_size):min(row,(i*block_size+block_size)),
+                (j*block_size):min(col,(j*block_size+block_size))
+            ]
+            blocked_rows.append(A)
+        blocked_matrix.append(blocked_rows)
+        
+    return blocked_matrix
 
+def stacking(blocked_matrix):
+    
+    rows = []
+    for i in range(len(blocked_matrix)):
+        rows.append(np.hstack(blocked_matrix[i]))
 
+    return np.vstack((rows))
 
+def emtpy_block(matrix):
+    empty = True
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            if matrix[i][j] != 0:
+                empty = False
+    if empty is True:
+        print("multiplied empty block", )
+        return True
+
+def naive_blocked_banded_mm(
+        A: np.ndarray, au: int, al: int,
+        B: np.ndarray, bu: int, bl: int,
+        block_size: int
+        ):
+    """ Naive banded matrix multiplication in blocks
+
+    param A np.ndarray: 
+    param ...
+
+    return C np.ndarray:
+    rtype: np.ndarrayd
+    """
+
+    blocked_A = blocking(A, 3)
+    blocked_B = blocking(B, 3)
+    blocked_C = [[0 for x in range(len(blocked_A))] for x in range(len(blocked_B[0]))]
+    
+    blocked_au = math.ceil(au/block_size)
+    blocked_bu = math.ceil(bu/block_size)
+    blocked_d_new = math.ceil(int((au + al + bu + bl)/2)/block_size)
+
+    m = len(blocked_B[0])
+    #print("j:", range(m))
+    for j in range(m):
+        #print("i:", range(max(0, j-blocked_d_new), min(m, j+blocked_d_new+1)))
+        for i in range(max(0, j-blocked_d_new), min(m, j+blocked_d_new+1)):
+            #print("k:", range(max(0,i-blocked_au, j-blocked_bu), min(m,i+blocked_au+1, j+blocked_bu+1)))
+            for k in range(max(0,i-blocked_au, j-blocked_bu), min(m,i+blocked_au+1, j+blocked_bu+1)):
+                if emtpy_block(blocked_A[i][k]):
+                    print(blocked_A[i][k])
+                if emtpy_block(blocked_B[k][j]):
+                    print(blocked_B[k][j])
+                blocked_C[i][j] += np.matmul(blocked_A[i][k],blocked_B[k][j])
+
+    return stacking(blocked_C)
+
+A = banded_matrix_generator(8, 3, 3)
+B = banded_matrix_generator(8, 2, 2)
+H = naive_blocked_banded_mm(A, 3, 3, B, 2, 2, 2)
+T = np.matmul(A,B)
+
+print("Diff\n", H-T)
 
 
 
