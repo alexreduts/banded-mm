023f72ebe2320132f04763e826de40c57c8b5095
Alex Studer <Alex.Studer@ibm.com>
2023-10-25 16:32:18 +0200

    Fixed gbmm_BLK implementation

--- src/gbmm.py
+++ src/gbmm.py
@@ -5,7 +5,7 @@ General banded matrix multiplication
 
 import numpy as np
 
-from matrix_utils import banded_matrix_generator
+from matrix_utils import banded_matrix_generator, binary_grid
 from naive_mm import naive_banded_mm
 
 def _gbmm_BLK_outer(
@@ -22,10 +22,10 @@ def _gbmm_BLK_outer(
 
     BL_ = slice(0, 0)
     BR_ = slice(BL_.stop, B.shape[1])
-
-    while C[CL_][:].shape[1] < C.shape[1]:
+    
+    while CL_.stop < C.shape[1]:
         # Tune for HW
-        c = 5
+        c = 2
 
         # Repartition
         C0_ = slice(CL_.start, CL_.stop)
@@ -37,7 +37,7 @@ def _gbmm_BLK_outer(
         B2_ = slice(B1_.stop, BR_.stop)
 
         # inner loop
-        C[:][C1_] = _gbmm_BLK_inner(C[:][C1_], A, B[:][B1_], ku, kl)
+        C[:, C1_] = _gbmm_BLK_inner(C[:, C1_], A, B[:, B1_], ku, kl)
 
         # Adjust partition
         CL_ = slice(C0_.start, C1_.stop)
@@ -45,6 +45,8 @@ def _gbmm_BLK_outer(
 
         BL_ = slice(B0_.start, B1_.stop)
         BR_ = slice(BL_.stop, B2_.stop)
+
+    return C
 
 
 def _gbmm_BLK_inner(
@@ -70,8 +72,7 @@ def _gbmm_BLK_inner(
     AL_ = slice(0, 0)
     AR_ = slice(AL_.stop, A.shape[1])
 
-    
-    while E[ET_][:].shape[0] < E.shape[0]:
+    while E[ET_, :].shape[0] < E.shape[0]:
         
         # Tune for Hardware
         b = 4
@@ -87,7 +88,7 @@ def _gbmm_BLK_inner(
 
         E0_ = slice(ET_.start, ET_.stop)
 
-        if D[D0_][:].shape[0] < (ku+1):
+        if D[D0_, :].shape[0] < (ku+1):
             #E1_ has 0 rows
             E1_ = slice(EM_.start, EM_.start)
             #A11 is emtpy
@@ -98,28 +99,29 @@ def _gbmm_BLK_inner(
             #A11 is bxb
             A1x_ = slice(AM_.start, AM_.start+b)
 
-        if D[D0_][:].shape[0] > (A.shape[1]-kl-1):
+            E[E1_, :] = E[E1_, :] + A[A1x_, Ax1_] @ D[D1_, :]
+
+        if D[D0_, :].shape[0] > (A.shape[1]-kl-1):
             #E3_ has 0 rows
             E3_ = slice(EB_.start, EB_.start)
             A3x_ = slice(AB_.start, AB_.start)
         else:
             #E3 has b rows
             E3_ = slice(EB_.start, EB_.start+b)
+            #A33 is bxb
             A3x_ = slice(AB_.start, AB_.start+b)
 
+            E[E3_, :] = E[E3_, :] + A[A3x_, Ax1_] @ D[D1_, :]
+
         E2_ = slice(E1_.stop, E3_.start)
-        A2x_ = slice(A1x_.stop, A3x_.stop)
+        A2x_ = slice(A1x_.stop, A3x_.start)
 
         E4_ = slice(E3_.stop, EB_.stop)
         A4x_ = slice(A3x_.stop, AB_.stop)
 
-        # Calculations
-        # triangular
-        E[E1_][:] = E[E1_][:] + A[A1x_][Ax1_] @ D[D1_][:]
-        # dense
-        E[E2_][:] = E[E1_][:] + A[A2x_][Ax1_] @ D[D1_][:]
-        # triangular
-        E[E3_][:] = E[E1_][:] + A[A3x_][Ax1_] @ D[D1_][:]
+        E[E2_, :] = E[E2_, :] + A[A2x_, Ax1_] @ D[D1_, :]
+
+        
 
         # Adjust partition
         ET_ = slice(E0_.start, E1_.stop)
@@ -133,8 +135,10 @@ def _gbmm_BLK_inner(
         AL_ = slice(0, Ax1_.stop)
         AR_ = slice(Ax2_.start, Ax2_.stop)
 
-        DT_ = slice(0)
+        DT_ = slice(0, D1_.stop)
+        DB_ = slice(DT_.stop, D2_.stop)
 
+    return E
 
 
     
@@ -148,4 +152,15 @@ def gbmm_BLK(
     C = _gbmm_BLK_outer(C, A, B, ku, kl)
     return C
 
-A = banded_matrix_generator(15)
+A = banded_matrix_generator(8, 2, 2)
+B = banded_matrix_generator(8, 2, 2)
+C = np.zeros((A.shape[0], B.shape[1]))
+H = gbmm_BLK(C, A, B, 2, 2)
+T = np.matmul(A,B)
+
+#print("H", H)
+#print("T", T)
+#print("Diff\n", H-T)
+binary_grid(H-T)
+assert np.allclose(H, T)
+
